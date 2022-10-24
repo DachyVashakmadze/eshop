38523763ff2e99ab438260ea2f2d1600e15edfce
mirian <mirian@apollo11.ge>
2022-10-25 00:52:40 +0400

    An example of simple angular routing

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,21 +1,24 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Product } from '../products.model';
 import { BaseProductService } from '../../services/base-product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   products: Product[] = [];
   columnCount = 0;
+  productSubscription!: Subscription;
+  breakpointSubscriptions: Subscription[] = [];
 
   constructor(private service: BaseProductService, private responseive: BreakpointObserver) { }
 
   ngOnInit(): void {
-    this.service.getProductList().subscribe(products => this.products = products);
+    this.productSubscription = this.service.getProductList().subscribe(products => this.products = products);
 
     if (this.responseive.isMatched([Breakpoints.Large, Breakpoints.XLarge])) {
       this.columnCount = 4;
@@ -33,32 +36,45 @@ export class ProductListComponent implements OnInit {
       this.columnCount = 1;
     }
 
-    this.responseive.observe([Breakpoints.Large, Breakpoints.XLarge])
-      .subscribe(result => {
-        if (result.matches) {
-          this.columnCount = 4;
-        }
-      });
+    this.breakpointSubscriptions.push(
+      this.responseive.observe([Breakpoints.Large, Breakpoints.XLarge])
+        .subscribe(result => {
+          if (result.matches) {
+            this.columnCount = 4;
+          }
+        })
+    );
 
-    this.responseive.observe(Breakpoints.Medium)
-      .subscribe(result => {
-        if (result.matches) {
-          this.columnCount = 3;
-        }
-      });
+    this.breakpointSubscriptions.push(
+      this.responseive.observe(Breakpoints.Medium)
+        .subscribe(result => {
+          if (result.matches) {
+            this.columnCount = 3;
+          }
+        })
+    );
 
-    this.responseive.observe(Breakpoints.Small)
-      .subscribe(result => {
-        if (result.matches) {
-          this.columnCount = 2;
-        }
-      });
+    this.breakpointSubscriptions.push(
+      this.responseive.observe(Breakpoints.Small)
+        .subscribe(result => {
+          if (result.matches) {
+            this.columnCount = 2;
+          }
+        })
+    );
 
-    this.responseive.observe(Breakpoints.XSmall)
-      .subscribe(result => {
-        if (result.matches) {
-          this.columnCount = 1;
-        }
-      });
+    this.breakpointSubscriptions.push(
+      this.responseive.observe(Breakpoints.XSmall)
+        .subscribe(result => {
+          if (result.matches) {
+            this.columnCount = 1;
+          }
+        })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.productSubscription.unsubscribe();
+    this.breakpointSubscriptions.forEach(Subscription => Subscription.unsubscribe());
   }
 }
--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductPersonalPageComponent } from '../product-personal-page/product-personal-page.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
-  {path: "products", component: ProductListComponent}
+  {path: "products", component: ProductListComponent},
+  {path: "product/:id", component: ProductPersonalPageComponent}
 ];
 
 @NgModule({
