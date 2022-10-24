41673d9596e9e84316a235143d5d585f7322ebe0
Dachy <iuzavashaqmadze@gmail.com>
2022-10-24 21:08:19 +0400

    trying add service in product-personal-page commponent try 2 :D

--- src/app/product-personal-page/product-personal-page.component.ts
+++ src/app/product-personal-page/product-personal-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseProductService } from '../services/base-product.service';
-  
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+    
 
 @Component({
   selector: 'app-product-personal-page',
@@ -8,16 +9,20 @@ import { BaseProductService } from '../services/base-product.service';
   styleUrls: ['./product-personal-page.component.css']
 })
 export class ProductPersonalPageComponent implements OnInit {
-  element ={
-    id: 1,
-    title: "Samsung Galaxy S12",
-    image: "https://m.media-amazon.com/images/I/71wGLBDEsvL._SX679_.jpg",
-    price: 1300
-  }
+  // element ={
+  //   id: 1,
+  //   title: "Samsung Galaxy S12",
+  //   image: "https://m.media-amazon.com/images/I/71wGLBDEsvL._SX679_.jpg",
+  //   price: 1300
+  // }
+  element =this.service.getProductById(1);
   displayedColumns: string[] = ['id', 'title', 'price'];
-  constructor(private service: BaseProductService, /*private responseive: BreakpointObserver*/) { }
+  constructor(private service: BaseProductService, private responseive: BreakpointObserver) { }
+  
 
   ngOnInit(): void {
+    // if( (this.element=this.service.getProductById(1))!=null )this.element=this.service.getProductById(1);
+    // this.service.getProductList().subscribe(products => this.products = products);
   }
 
 }
