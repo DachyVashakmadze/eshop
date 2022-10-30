50f96a8a708f6ca7b7ae32f6f7dc23359fd92b8b
mirian <mirian@apollo11.ge>
2022-10-30 21:18:12 +0400

    Initial page layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductPersonalPageComponent } from './product-personal-page/product-personal-page.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'product/:id', component: ProductPersonalPageComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: '', component: ProductListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,30 @@ import { TestProductService } from './services/test-product.service';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { ProductModule } from './product/product.module';
+import { MenuComponent } from './menu/menu.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FooterComponent } from './footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
     MatSliderModule,
-    ProductModule
+    ProductModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule
   ],
   providers: [
     {
