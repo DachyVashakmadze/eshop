4c45aad6f6062c1ca4f4fa59063867bb65265526
Dachi Vashakmadze <iuzavashaqmadze@gmail.com>
2022-11-25 21:46:02 +0400

    building carusel

--- src/app/product/product-carusel/product-carusel.component.ts
+++ src/app/product/product-carusel/product-carusel.component.ts
@@ -23,7 +23,12 @@ export class ProductCaruselComponent implements OnInit {
     "https://i.pcmag.com/imagery/reviews/066u20pOsJx1BG4PfEAD0RB-24..v1665583071.jpg",
     "https://imgix.bustle.com/uploads/image/2022/10/12/c81e59c2-8e6e-4217-b80e-28005ccdaeaf-dsc00543-2.jpg?w=2000&h=980&fit=crop&crop=faces&auto=format%2Ccompress",
     "https://mobiledrop.in/wp-content/uploads/2022/10/Google-Pixel-7-Pro-Review-Images-01.jpg",
-    "https://www.google.com/search?q=pixel+7+pro&sxsrf=ALiCzsY6Br_HkvrtMdpRdTk-xMOyDy45TA:1668784166885&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjd9eS7gbj7AhXqRvEDHXbYBs0Q_AUoAnoECAIQBA&biw=1650&bih=963&dpr=1#imgrc=FxjQxwKTKiTuaM"
+    "https://media.wired.com/photos/633dc4042d9f11bddfef6a1a/master/w_1600%2Cc_limit/Google-Pixel-7-All-Colors-Gear.jpg",
+    "https://www.91-cdn.com/hub/wp-content/uploads/2022/09/Pixel-6a-1.jpg",
+    "https://specs-tech.com/wp-content/uploads/2021/11/Google-Pixel-7a.jpg",
+    "https://www.ixbt.com/img/n1/news/2022/9/0/pixel_large.JPG",
+    "https://www.electrorates.com/blogimg/Google_Pixel_7a.png",
+    "https://telecomtalk.info/wp-content/uploads/2022/10/google-pixel-7a-launch-is-near-amazon.jpeg"
   ];
 
   constructor() { }
@@ -49,10 +54,52 @@ export class ProductCaruselComponent implements OnInit {
     this.fixArraySizeError();
     console.log("tryed to get sourse");
   }
-
   fixArraySizeError(): void{ //FUC, this function will check if picture array size is more than pictureSource, if so multiplyes content in pictureSource
     console.log("tring to get sourse, cheking for size error");
   }
 
+  perviousButton(): void{ // clicking picture meniu button "pervious"
+    if(this.manePicture!=this.pictureSource[0]){
+      //this button should not work if we are at the start of product pictures
+      if(this.picturMenuPointer>0 ){
+        //if you have pervious picture in pictures mini meniu
+        this.picturMenuPointer--;
+        this.manePicture=this.pictures[this.picturMenuPointer];
+      }else if(this.sourseArrayPointer>0){
+        //if you do not have pervious picture in pictures mini meniu, but you have more in sourse 
+        this.sourseArrayPointer--;
+        this.fillPictures();
+        //this.manePicture=this.pictures[0];
+        this.changeManePicture(this.picturMenuPointer);
+      }
 
+    }else if(this.sourseArrayPointer<0){//temporery
+      console.log("ERROR!!! picture menius pointer is negative number");
+    }
+  }
+
+  nextButton(): void{ // clicking picture meniu button "next"
+    if(this.manePicture!=this.pictureSource[this.pictureSource.length-1]){
+      //this button should not work if we are at the end of product pictures
+      if(this.manePicture!=this.pictures[this.caruselMeniuSize-1]){
+        //if you have next picture in pictures mini meniu
+        this.picturMenuPointer++;
+        this.manePicture=this.pictures[this.picturMenuPointer];
+      }else if(this.sourseArrayPointer<(this.pictureSource.length-1)){
+        //if you do not have next picture in pictures mini meniu, but you have more in sourse
+        this.sourseArrayPointer++;
+        this.fillPictures();
+        //this.manePicture=this.pictures[this.caruselMeniuSize-1];
+        this.changeManePicture(this.picturMenuPointer);
+      }
+
+    }else if(this.sourseArrayPointer>(this.pictureSource.length-this.caruselMeniuSize)){//temporery
+      console.log("ERROR!!! picture menius pointer out of bound");
+    }
+  }
+
+  changeManePicture(index: number): void{
+    this.picturMenuPointer=index;
+    this.manePicture=this.pictures[index];
+  }
 }
