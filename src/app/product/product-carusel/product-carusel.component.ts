c8d57dbb2c0fab7c90af626ca25b8fde296e3a68
Dachi Vashakmadze <iuzavashaqmadze@gmail.com>
2022-12-03 21:47:25 +0400

    impruving carusel

--- src/app/product/product-carusel/product-carusel.component.ts
+++ src/app/product/product-carusel/product-carusel.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ThemeableComponent } from 'src/app/common/theamable.component';
-
+import { ThemingService } from 'src/app/services/theming.service';
+import { BaseProductService } from 'src/app/services/base-product.service';
+import { Product } from '../products.model';
 @Component({
   selector: 'app-product-carusel',
   templateUrl: './product-carusel.component.html',
@@ -9,30 +12,37 @@ import { ThemeableComponent } from 'src/app/common/theamable.component';
 export class ProductCaruselComponent extends ThemeableComponent implements OnInit {
   manePicture: string =""; //at start, mane picture should be first picture in array, see (*1)
 
-  @Input() caruselMeniuSize: number=4; //UC (under construction), there should not be 4 here !!!, pearent should be able to set number
+  @Input() caruselMeniuSize: number=4; //pearent will set number, defult is 4
+  @Input() imgesSource: string[]=[];; //pearent will give string [] for pictures, defult is null
   picturMenuPointer: number=0; //this number shows were should be focus, in picture menu list
   sourseArrayPointer: number=0; //this number points spot from we should start coping picturs
 
   contentError: string="https://image.shutterstock.com/image-vector/no-image-available-icon-flat-260nw-1240855801.jpg";//error picture, for no content
   pictures: string [] = [];
-  //picturesSorce=[]; //UC, for future 
-  pictureSource =[
-    "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Hero_Image_Thumbnail.max-1000x1000.jpg",
-    "https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Google_Pixel_7_Ultra_Pixel_7_Pixel_7_Pro_Pixel_Fold_Pixel_Tablet_Kamera_Details_im_Code_entdeckt.jpeg",
-    "https://fdn.gsmarena.com/imgroot/news/22/10/pixel-7-7pro-official/inline2/-1200/gsmarena_003.jpg",
-    "https://9to5toys.com/wp-content/uploads/sites/5/2022/10/google-pixel-7-pro-1.jpg",
-    "https://i.pcmag.com/imagery/reviews/066u20pOsJx1BG4PfEAD0RB-24..v1665583071.jpg",
-    "https://imgix.bustle.com/uploads/image/2022/10/12/c81e59c2-8e6e-4217-b80e-28005ccdaeaf-dsc00543-2.jpg?w=2000&h=980&fit=crop&crop=faces&auto=format%2Ccompress",
-    "https://mobiledrop.in/wp-content/uploads/2022/10/Google-Pixel-7-Pro-Review-Images-01.jpg",
-    "https://media.wired.com/photos/633dc4042d9f11bddfef6a1a/master/w_1600%2Cc_limit/Google-Pixel-7-All-Colors-Gear.jpg",
-    "https://www.91-cdn.com/hub/wp-content/uploads/2022/09/Pixel-6a-1.jpg",
-    "https://specs-tech.com/wp-content/uploads/2021/11/Google-Pixel-7a.jpg",
-    "https://www.ixbt.com/img/n1/news/2022/9/0/pixel_large.JPG",
-    "https://www.electrorates.com/blogimg/Google_Pixel_7a.png",
-    "https://telecomtalk.info/wp-content/uploads/2022/10/google-pixel-7a-launch-is-near-amazon.jpeg"
-  ];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: BaseProductService,
+    protected override themingService: ThemingService
+  ) {
+    super(themingService);
+  }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = params["id"]
+      this.service.getProductById(id).subscribe(p => {
+        if (!p) {
+          this.router.navigate(["urlNotFound"], { skipLocationChange: true })
+        } else {
+          if(p.images.length>0){
+            this.imgesSource=p.images;
+            if(this.caruselMeniuSize>p.images.length) this.caruselMeniuSize=p.images.length; //if there is less images than carusel needs reduce size
+          }
+        }
+      });
+    })
     this.getSorce();
     this.fillPictures();
     if(this.pictures.length>0){//(*1), at start, mane picture should be first picture in array
@@ -45,7 +55,7 @@ export class ProductCaruselComponent extends ThemeableComponent implements OnIni
   fillPictures(): void{// fills picture array, from source
     this.pictures = [] ;
     for(let i=0; i<this.caruselMeniuSize; i++){
-      this.pictures[i]=this.pictureSource[this.sourseArrayPointer+i];
+      this.pictures[i]=this.imgesSource[this.sourseArrayPointer+i];
     }     
   }
 
@@ -58,7 +68,7 @@ export class ProductCaruselComponent extends ThemeableComponent implements OnIni
   }
 
   perviousButton(): void{ // clicking picture meniu button "pervious"
-    if(this.manePicture!=this.pictureSource[0]){
+    if(this.manePicture!=this.imgesSource[0]){
       //this button should not work if we are at the start of product pictures
       if(this.picturMenuPointer>0 ){
         //if you have pervious picture in pictures mini meniu
@@ -78,13 +88,13 @@ export class ProductCaruselComponent extends ThemeableComponent implements OnIni
   }
 
   nextButton(): void{ // clicking picture meniu button "next"
-    if(this.manePicture!=this.pictureSource[this.pictureSource.length-1]){
+    if(this.manePicture!=this.imgesSource[this.imgesSource.length-1]){
       //this button should not work if we are at the end of product pictures
       if(this.manePicture!=this.pictures[this.caruselMeniuSize-1]){
         //if you have next picture in pictures mini meniu
         this.picturMenuPointer++;
         this.manePicture=this.pictures[this.picturMenuPointer];
-      }else if(this.sourseArrayPointer<(this.pictureSource.length-1)){
+      }else if(this.sourseArrayPointer<(this.imgesSource.length-1)){
         //if you do not have next picture in pictures mini meniu, but you have more in sourse
         this.sourseArrayPointer++;
         this.fillPictures();
@@ -92,7 +102,7 @@ export class ProductCaruselComponent extends ThemeableComponent implements OnIni
         this.changeManePicture(this.picturMenuPointer);
       }
 
-    }else if(this.sourseArrayPointer>(this.pictureSource.length-this.caruselMeniuSize)){//temporery
+    }else if(this.sourseArrayPointer>(this.imgesSource.length-this.caruselMeniuSize)){//temporery
       console.log("ERROR!!! picture menius pointer out of bound");
     }
   }
