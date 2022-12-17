98ab49ac267b56e476dcc059996869aaedb0e88a
mirian <mirian@apollo11.ge>
2022-12-17 21:44:39 +0400

    Add product link on shopping cart page

--- src/app/cart/cart-list-item/cart-list-item.component.ts
+++ src/app/cart/cart-list-item/cart-list-item.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ThemeableComponent } from 'src/app/common/theamable.component';
-import { Product } from 'src/app/product/products.model';
 import { BaseCartService } from 'src/app/services/base-cart.service';
-import { BaseProductService } from 'src/app/services/base-product.service';
 import { ThemingService } from 'src/app/services/theming.service';
 import { CartItem } from '../cart-item.model';
 
